1821c1c9508be7ee6b9ffa2fb9d33288a14a2d2b
lisaLe <lisale841@gmail.com>
2022-10-20 15:06:14 -0700

    pulled results from API to current weather page

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -11,8 +11,8 @@ const Weather = ({state}) => {
     <div className = 'homeBackground'>
      <div className= 'row rowSpacing'>
       <div className= 'locationText textStyle'>
-        Westminster <br/>
-        California 
+      {state && state?.location?.name}<br/>
+      {state && state?.location?.region}
       </div>
       <div className='circle'>
         <img src ={require('../../images/sunny-cat.jpg')} alt='cat-doodle' className= 'imagesCircle'/>
@@ -20,10 +20,22 @@ const Weather = ({state}) => {
       <div className ='logoFont'>
         WEATHER-MEOW
       </div>
+      <div className ='tempText textStyle'>
+      {state && state?.current?.temp_f}°
+      </div>
+      <div className ='logoFont'>
+      {state && state?.current?.condition?.text}
+      <img src = {state && state?.current?.condition?.icon} alt='weatherIcon' />
+     
+      </div>
+      {/* <div className ='logoFont'>
+      <img src = {state && state?.current?.condition?.icon} alt='weatherIcon' className= 'imagesCircle'/>
+      {state && state?.current?.condition?.icon}
+      </div> */}
       <div>
-        {state && state?.current?.temp_f}
+        {/* {state && state?.current?.temp_f}
         {state && state?.current?.wind_mph}
-        {state && state?.current?.feelslike_f}
+        {state && state?.current?.feelslike_f} */}
       </div>
       
      </div>
